0ef6ca06ef4b1ed2d7b0b1b225d0fedc2ba582ed
scrymee <syoumee@gmail.com>
2022-08-18 23:41:49 +0900

    updateDeployShell

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo 'コミットメッセージをupdateに設定します'
+    MSG='update'
+    exit 1
+else
+    MSG=$1
+fi
+echo $MSG
+
 # ローカルのgitに修正を反映する
 git add . 
-git commit -m 'update'
-
+git commit -m "$MSG"
 # mainブランチを指定リポジトリのsrcブランチにpushする
-git push -u main:src  
+git push -u origin main:src
 
 
 # # リモートのファイルを一旦削除して
